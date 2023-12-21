a9ab40b19b005590180ccbbbbbd27cc20e2a3794
rachna2909 <149859735+rachna2909@users.noreply.github.com>
2023-12-21 15:23:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,15 +53,6 @@ def grayscale(image_path):
     grayscale_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
     return grayscale_image
 
-def apply_contrast_adjustment(image_path):
-    img = cv2.imread(image_path)
-    adjust = 50
-    adjust = max(adjust, -100)
-    contrast_factor = np.power((adjust + 100) / 100, 2)
-    img = ((img / 255 - 0.5) * contrast_factor + 0.5) * 255
-    img = np.clip(img, 0, 255).astype(np.uint8)
-    return img
-
 def apply_brightness_adjustment(image_path):
     img = cv2.imread(image_path)
     adjust=50
@@ -70,27 +61,6 @@ def apply_brightness_adjustment(image_path):
     img = np.clip(img, 0, 255).astype(np.uint8)
     return img
 
-def adjust_hue(image_path):
-    original_image = cv2.imread(image_path)
-    adjust = 30
-    hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
-    hsv_image[:, :, 0] = (hsv_image[:, :, 0] + adjust) % 180
-    adjusted_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-    return adjusted_image
-
-def adjust_saturation(image_path):
-    original_image = cv2.imread(image_path)
-    adjust = 20
-    adjust_factor = adjust * -0.01
-    rgb_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    max_array = np.max(rgb_image, axis=-1, keepdims=True)
-    sat_array = np.where(
-        rgb_image != max_array,
-        rgb_image + (max_array - rgb_image) * adjust_factor,
-        rgb_image,
-    )
-    adjusted_image = cv2.cvtColor(sat_array.astype(np.uint8), cv2.COLOR_RGB2BGR)
-    return adjusted_image
     
 @app.get("/")
 def root():
@@ -124,15 +94,9 @@ async def upload_image(filter_name: str, file: UploadFile = File(...)):
         elif filter_name == "sepia":
             processed_image = (apply_sepia_filter(upload_path))
         elif filter_name == "grayscale":
-            processed_image = (grayscale(upload_path))
-        elif filter_name == "contrast":
-            processed_image = (apply_contrast_adjustment(upload_path))    
+            processed_image = (grayscale(upload_path))  
         elif filter_name == "bright":
-            processed_image = (apply_brightness_adjustment(upload_path))
-        elif filter_name == "hue":
-            processed_image = (adjust_hue(upload_path))    
-        elif filter_name == "saturation":
-            processed_image = (adjust_saturation(upload_path))    
+            processed_image = (apply_brightness_adjustment(upload_path))  
         else:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
